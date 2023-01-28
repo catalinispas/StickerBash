f2ad6a6ec3af3178214801e6aec0755a30fa83f9
Catalin Ispas <70150418+catalinispas@users.noreply.github.com>
2023-01-28 23:33:01 +0200

    implemented image picker and a view that displays the image

--- App.js
+++ App.js
@@ -1,19 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import { useState } from 'react';
+
+// Components Imports
 import ImageViewer from './components/ImageViewer';
+import Button from './components/Button';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer placeholderImageSource={PlaceholderImage} />
+        <ImageViewer
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
-      ;
-      <Text style={{ color: '#fff' }}>
-        Open up App.js to start working on your app!
-      </Text>
+
+      <View style={styles.footerContainer}>
+        <Button
+          theme='secondary'
+          onPress={pickImageAsync}
+          label='Choose a photo'
+        />
+        <Button label='Use this photo' />
+      </View>
       <StatusBar style='auto' />
     </View>
   );
@@ -28,5 +57,9 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
     paddingTop: 32,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
