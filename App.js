67faa1f1c5f4793563a2dc693bd886cfd6951c73
Catalin Ispas <70150418+catalinispas@users.noreply.github.com>
2023-02-01 00:25:32 +0200

    fixed background on splash screen

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 // Libraries
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { useRef, useState } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import * as MediaLibrary from 'expo-media-library';
 import { captureRef } from 'react-native-view-shot';
+import domtoimage from 'dom-to-image';
 
 // Components Imports
 import ImageViewer from './components/ImageViewer';
@@ -54,23 +55,45 @@ export default function App() {
     setIsModalVisible(true);
     // TODO Later
   };
+
+  // Image Capture
   const onSaveImageAsync = async () => {
-    try {
-      const localUri = await captureRef(imageRef, { height: 440, quality: 1 });
-
-      await MediaLibrary.saveToLibraryAsync(localUri);
-
-      if (localUri) {
-        alert('Saved!');
+    if (Platform.OS !== 'web') {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert('Saved!');
+        }
+      } catch (e) {
+        console.log(e);
       }
-    } catch (e) {
-      console.log(e);
+    } else {
+      domtoimage
+        .toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        })
+        .then(dataUrl => {
+          let link = document.createElement('a');
+          link.download = 'sticker-smash.jpeg';
+          link.href = dataUrl;
+          link.click();
+        })
+        .catch(e => {
+          console.log(e);
+        });
     }
   };
   const onModalClose = () => {
     setIsModalVisible(false);
   };
   const imageRef = useRef();
+
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
@@ -106,7 +129,7 @@ export default function App() {
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
-      <StatusBar style='auto' />
+      <StatusBar style='light' />
     </GestureHandlerRootView>
   );
 }
